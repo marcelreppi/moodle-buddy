9ebe956fc960d1c33f12dad001965d12003de82e
Marcel Reppenhagen <marcelreppi@gmail.com>
2019-12-16 04:04:47 +0100

    fix missing browser. fix illegal file due to query

--- extension/background_scripts/downloader.js
+++ extension/background_scripts/downloader.js
@@ -13,7 +13,11 @@ browser.runtime.onMessage.addListener(message => {
   }
 
   if (message.command === "download-file") {
-    let filename = message.url.split("/").pop()
+    let filename = message.url
+      .split("/")
+      .pop() // Take last part of URL
+      .split("?")
+      .shift() // Take everything before query parameters
     const filenameParts = filename.split(".")
     const fileType = filenameParts[filenameParts.length - 1]
 
