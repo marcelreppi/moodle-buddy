9e6d35332b8a05a48b3011a7707ba086186f7588
Marcel Reppenhagen <marcelreppi@gmail.com>
2019-12-25 22:56:49 +0100

    fix formatting. remove browser check.

--- extension/pages/information/information.js
+++ extension/pages/information/information.js
@@ -1,7 +1,6 @@
 document.querySelector("#imprint-link").addEventListener("click", e => {
-  const isFirefox = typeof InstallTrigger !== "undefined"
   browser.tabs.create({
-    url: isFirefox ? "../legal/legal.html" : "pages/legal/legal.html",
+    url: "pages/legal/legal.html",
   })
 })
 
