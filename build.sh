74c1456d72726d1fdf1525c36676e5754ba2adee
Marcel Reppenhagen <marcelreppi@gmail.com>
2019-12-27 02:58:56 +0100

    publish 1.1

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
+rm moodle-buddy.zip
+rm moodle-buddy-code.zip
+
 npm run build
 
+# Make extension zip
 cd build
-zip -r ../moodle-buddy ./*
+zip -r ../moodle-buddy .
+cd ..
+
+# Make zip of all the code for updating the extension
+zip -r ./moodle-buddy-code extension webpack.config.js package.json README.md
