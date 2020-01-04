5b317e931b247eff7d1b0a8191a3ba1796aebdf4
Marcel Reppenhagen <marcelreppi@gmail.com>
2020-01-04 21:22:50 +0100

    remove folder

--- events/reset.sh
+++
@@ -1,6 +0,0 @@
-echo "event;date;time;browser;browserId" > events.csv
-
-aws s3 rm s3://moodle-buddy-event-bucket/events.csv
-aws s3 cp ./events.csv s3://moodle-buddy-event-bucket/events.csv
-
-rm events.csv
--- events/test/download.sh
+++
@@ -1,4 +0,0 @@
-rm events.csv
-
-# Download from S3
-aws s3 cp s3://moodle-buddy-event-bucket-test/events.csv events.csv
