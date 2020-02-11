85257f5f02e34d5d0b5a72fe1c3a2e6e0a151bd7
Marcel Reppenhagen <marcelreppi@gmail.com>
2020-02-11 22:47:33 +0100

    fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,4 +11,4 @@ zip -r ../moodle-buddy .
 cd ..
 
 # Make zip of all the code for updating the extension
-zip -r ./moodle-buddy-code extension webpack.config.js package.json README.md
+zip -r ./moodle-buddy-code extension screenshots webpack.config.js package.json package-lock.json
