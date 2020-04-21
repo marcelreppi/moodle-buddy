813d4cbe49f41732f6a251f59c1f3b6c31cb7069
Marcel Reppenhagen <marcelreppi@gmail.com>
2020-04-21 11:22:24 +0200

    add 32 pixel icon

--- extension/background_scripts/helpers.js
+++ extension/background_scripts/helpers.js
@@ -99,8 +99,8 @@ export async function setIcon(tabId) {
     browser.browserAction.setIcon({
       path: {
         16: "/icons/16.png",
+        32: "/icons/32.png",
         48: "/icons/48.png",
-        128: "/icons/128.png",
       },
       tabId,
     })
