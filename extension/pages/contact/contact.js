844f84eeaf067f8adb73b1afed0e23140df9f70b
Marcel Reppenhagen <marcelreppi@gmail.com>
2020-09-21 21:46:13 +0200

    add moodle platform to feedback form

--- extension/pages/contact/contact.js
+++ extension/pages/contact/contact.js
@@ -1,16 +1,25 @@
-document.querySelector("#form-button").addEventListener("click", () => {
+document.querySelector("#form-button").addEventListener("click", async () => {
+  const platform = document.querySelector("#form-platform").value
   const subject = document.querySelector("#form-subject").value
   const content = document.querySelector("#form-content").value
 
-  if (subject !== "" && content !== "") {
+  if (subject !== "" && content !== "" && platform !== "") {
+    const { browserId } = await browser.storage.local.get("browserId")
+
+    const message = [`Moodle platform: ${platform}`, `Browser ID: ${browserId}`, content].join(
+      "\n\n"
+    )
+
     browser.runtime.sendMessage({
       command: "feedback",
       subject,
-      content,
+      content: message,
     })
 
-    document.querySelector(".form-container").style.display = "none"
+    document.querySelector(".section-content").style.display = "none"
     document.querySelector(".success").style.display = "flex"
+  } else {
+    document.querySelector(".missing").style.display = "block"
   }
 })
 
