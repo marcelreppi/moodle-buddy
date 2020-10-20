c4ba2b6fc3bb2258bb6b9d6faedbf860f7a27754
Marcel Reppenhagen <marcelreppi@gmail.com>
2020-10-20 19:31:04 +0200

    debug bug fix

--- extension/background_scripts/extension-listener.js
+++ extension/background_scripts/extension-listener.js
@@ -118,7 +118,7 @@ browser.runtime.onMessage.addListener(async (message, sender) => {
       break
     case "debug":
       browser.tabs.executeScript({
-        file: `../content_scripts/${message.page}-page.js`,
+        file: `content_scripts/${message.page}-page.js`,
       })
       break
     default:
