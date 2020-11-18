67b64dac33ecfb60a182e561097a93f70b8a91bc
Marcel Reppenhagen <marcelreppi@gmail.com>
2020-11-18 17:17:07 +0100

    add testing

---
+++ test/index.js
@@ -0,0 +1,78 @@
+const fs = require("fs")
+const { JSDOM } = require("jsdom")
+const { default: Course } = require("../extension/models/Course")
+
+const dir = "files"
+const files = fs.readdirSync(dir)
+// const files = ["scan_course_2020-11-03T19-11-59.html"]
+
+const defaultOptions = {
+  onlyNewResources: false,
+  useMoodleFileName: true,
+  showDownloadOptions: false,
+  prependCourseShortcutToFileName: false,
+  prependCourseToFileName: false,
+  alwaysShowDetails: false,
+  disableInteractionTracking: false,
+  defaultMoodleURL: "",
+  autoSetMoodleURL: true,
+  backgroundScanInterval: 30,
+  enableBackgroundScanning: true,
+  downloadFolderAsZip: true,
+  saveToMoodleFolder: false,
+  folderStructure: "CourseFile",
+  includeVideo: true,
+  includeAudio: true,
+  includeImage: false,
+}
+
+async function run() {
+  for (const file of files) {
+    console.log("Checking", file)
+    const path = `${dir}/${file}`
+    const fileContent = fs.readFileSync(path, "utf8")
+    const dom = new JSDOM(fileContent)
+    const { document } = dom.window
+    const course = new Course(path, document)
+    const localStorage = { options: defaultOptions }
+
+    let error = false
+
+    await course.scan(localStorage)
+
+    if (course.name === "Unknown Course") {
+      console.log("Unknown Course")
+      error = true
+    }
+
+    if (course.shortcut === "Unknown Shortcut") {
+      console.log("Unknown Shortcut")
+      error = true
+    }
+
+    const { resourceNodes } = course
+    for (const r of resourceNodes) {
+      if (r.fileName === "Unknown Filename") {
+        console.log("Unknown Filename")
+        error = true
+      }
+
+      if (r.section === "Unknown Section") {
+        console.log("Unknown Section")
+        error = true
+      }
+
+      if (error) {
+        break
+      }
+    }
+    if (error) {
+      console.log(course.link)
+      return
+    }
+  }
+
+  console.log("All good!")
+}
+
+run()
