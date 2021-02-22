30852ea9a80fb8a54d57a952b23b07a1dd21d585
Marcel Reppenhagen <marcelreppi@gmail.com>
2021-02-22 17:13:45 +0100

    prettier

--- extension/pages/update/update.js
+++ extension/pages/update/update.js
@@ -1,18 +1,16 @@
-document
-  .querySelectorAll(".version")
-  .forEach((node) => (node.textContent = browser.runtime.getManifest().version))
+document.querySelectorAll(".version").forEach(node => {
+  node.textContent = browser.runtime.getManifest().version
+})
 
-document.querySelectorAll(".info-page").forEach((node) => {
+document.querySelectorAll(".info-page").forEach(node => {
   node.addEventListener("click", () => {
     browser.tabs.create({
-      url: `/pages/information/information.html${
-        node.id !== "" ? `#${node.id}` : ""
-      }`,
+      url: `/pages/information/information.html${node.id !== "" ? `#${node.id}` : ""}`,
     })
   })
 })
 
-document.querySelectorAll(".options-link").forEach((node) => {
+document.querySelectorAll(".options-link").forEach(node => {
   node.addEventListener("click", () => {
     browser.tabs.create({
       url: `/pages/options/options.html${node.id !== "" ? `#${node.id}` : ""}`,
