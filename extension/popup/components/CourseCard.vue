f6c15a702c0b89f7a3283f5d2e4d95440725aa36
Marcel Reppenhagen <marcelreppi@gmail.com>
2021-04-24 18:55:19 +0200

    rewrite to tailwindcss

--- extension/popup/components/CourseCard.vue
+++ extension/popup/components/CourseCard.vue
@@ -1,55 +1,79 @@
 <template>
-  <div class="course-card">
-    <div class="course-name">
-      <div :class="{ 'update-name': hasUpdates }">{{ course.name }}</div>
-      <div class="course-link action" @click="() => onCourseLinkClick(course.link)">
+  <div class="flex flex-col px-4 py-3 my-2 border border-gray-200 rounded-md">
+    <div class="flex items-start justify-between">
+      <div>
+        {{ course.name }}
+        <div v-if="hasUpdates" class="inline-block w-1.5 h-1.5 mb-2 rounded-full bg-mb-red">
+          <!-- Dot -->
+        </div>
+      </div>
+      <div
+        class="self-start h-full ml-1 mt-0.5 text-sm text-gray-600 whitespace-nowrap custom-hover"
+        @click="() => onCourseLinkClick(course.link)"
+      >
         Go to course
       </div>
     </div>
 
     <div v-if="hasUpdates">
-      <div class="course-details">
-        <div v-if="newResources.length > 0" class="update-details">
+      <div class="mt-1 text-xs text-gray-700">
+        <div v-if="newResources.length > 0" class="font-bold">
           <span>{{ course.counts.nNewFiles + course.counts.nNewFolders }}</span>
-          <span v-if="course.counts.nNewFiles + course.counts.nNewFolders === 1">new resource</span>
-          <span v-else>new resources</span>
+          {{ " " }}
+          <span>
+            {{ course.counts.nNewActivities === 1 ? "new resource" : "new resources" }}
+          </span>
         </div>
-        <div v-if="newActivities.length > 0" class="update-details">
+        <div v-if="newActivities.length > 0" class="font-bold">
           <span>{{ course.counts.nNewActivities }}</span>
-          <span v-if="course.counts.nNewActivities === 1">new activity</span>
-          <span v-else>new activities</span>
+          {{ " " }}
+          <span>
+            {{ course.counts.nNewActivities === 1 ? "new activity" : "new activities" }}
+          </span>
         </div>
-        <div class="detail-switch" @click="onDetailClick">
-          <div ref="arrow" class="arrow" />
-          <div class="switch-text">{{ showDetails ? "Hide" : "Show" }} details</div>
+        <div class="flex items-center ml-1 mt-0.5" @click="onDetailClick">
+          <div
+            ref="arrow"
+            class="transition-all mr-1.5 -ml-1 w-1.5 h-1.5 border-t-2 border-black border-r-2 transform"
+            :class="showDetails ? 'rotate-[135deg] -mt-0.5' : 'rotate-45'"
+          />
+          <div class="custom-hover">{{ showDetails ? "Hide" : "Show" }} details</div>
         </div>
-        <div v-if="showDetails" class="detail-container">
+        <div v-if="showDetails" class="pl-4 mt-1 text-xs text-gray-600">
           <div v-for="(node, i) in allNewNodes" :key="i">
-            <span v-if="node.isFile" class="filename">- {{ node.name }} (File)</span>
-            <span v-if="node.isFolder" class="filename">- {{ node.name }} (Folder)</span>
-            <span v-if="node.isActivity" class="filename">
+            <span v-if="node.isFile" class="break-all">- {{ node.name }} (File)</span>
+            <span v-if="node.isFolder" class="break-all">- {{ node.name }} (Folder)</span>
+            <span v-if="node.isActivity" class="break-all">
               - {{ node.activityName }} (Activity)
             </span>
           </div>
         </div>
       </div>
 
-      <div class="download-row">
-        <div class="action" @click="() => onMarkAsSeenClick(course)">Mark as seen</div>
+      <div class="flex flex-row-reverse items-center justify-between">
+        <div
+          class="mt-1.5 text-sm text-gray-600 whitespace-nowrap custom-hover"
+          @click="() => onMarkAsSeenClick(course)"
+        >
+          Mark as seen
+        </div>
         <button
           v-if="newResources.length > 0"
-          class="download-button"
+          class="mt-2 text-sm font-bold btn"
           @click="e => onDownloadClick(e, course)"
         >
           Download new resources
         </button>
       </div>
     </div>
-    <div v-else-if="course.isNew" class="course-details">
-      Scanned for the first time.
-      <br />Updates will be shown on your next visit.
+    <div v-else class="mt-1 text-xs text-gray-600">
+      <div v-if="course.isNew">
+        Scanned for the first time.
+        <br />
+        Updates will be shown on your next visit.
+      </div>
+      <div v-else>No new updates</div>
     </div>
-    <div v-else class="course-details">No new updates</div>
   </div>
 </template>
 
@@ -105,18 +129,6 @@ export default defineComponent({
       )
     },
   },
-  watch: {
-    showDetails(value) {
-      const arrow = this.$refs.arrow as HTMLDivElement
-      if (value) {
-        arrow.style.marginTop = "-2px"
-        arrow.style.transform = "rotate(135deg)"
-      } else {
-        arrow.style.marginTop = "0px"
-        arrow.style.transform = "rotate(45deg)"
-      }
-    },
-  },
   mounted() {
     if (!this.options) return
 
@@ -166,119 +178,4 @@ export default defineComponent({
 })
 </script>
 
-<style scoped>
-.course-card {
-  border: 1px rgb(230, 230, 230) solid;
-  border-radius: 5px;
-  padding: 10px 15px;
-  margin: 7px 0px;
-  display: flex;
-  flex-direction: column;
-}
-
-.course-name {
-  display: flex;
-  align-items: flex-start;
-  justify-content: space-between;
-}
-
-.course-link {
-  margin-left: 5px;
-}
-
-.action:hover {
-  color: #c50e20;
-  cursor: pointer;
-  text-decoration: underline;
-}
-
-.action {
-  white-space: nowrap;
-  font-size: 14px;
-  align-self: flex-end;
-  color: rgb(66, 66, 66);
-}
-
-.update-name::after {
-  content: "*";
-  margin-left: 2px;
-  vertical-align: super;
-  color: #c50e20;
-}
-
-.course-details {
-  font-size: 12px;
-  color: rgb(57, 57, 57);
-  margin-top: 5px;
-}
-
-.update-details {
-  font-weight: bold;
-}
-
-.detail-container {
-  margin-top: 5px;
-  padding-left: 15px;
-}
-
-.filename {
-  word-break: break-all;
-}
-
-.download-row {
-  display: flex;
-  align-items: center;
-  flex-direction: row-reverse;
-  justify-content: space-between;
-}
-
-.download-button {
-  /* width: 170px; */
-  padding: 10px 10px;
-  margin-top: 10px;
-  border-radius: 5px;
-  border: 0;
-  background-color: #c50e20;
-  color: white;
-  font-size: 11px;
-  font-weight: bold;
-  text-align: center;
-  letter-spacing: 0.7px;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-}
-
-.download-button:hover {
-  cursor: pointer;
-  text-decoration: underline;
-}
-
-.download-button:disabled {
-  background-color: #a8a8a8;
-  cursor: default;
-  text-decoration: none;
-}
-
-.detail-switch {
-  display: flex;
-  align-items: center;
-  margin-left: 5px;
-  margin-top: 3px;
-}
-
-.switch-text:hover {
-  color: #c50e20;
-  cursor: pointer;
-  text-decoration: underline;
-}
-
-.arrow {
-  transition: all 0.5s;
-  margin-right: 6px;
-  margin-left: -5px;
-  width: 4px;
-  height: 4px;
-  border-top: 2px solid black;
-  border-right: 2px solid black;
-  transform: rotate(45deg);
-}
-</style>
+<style></style>
