f599c594f84077e8e3960b2ff4c435d87ff5fca7
Marcel Reppenhagen <marcelreppi@gmail.com>
2021-11-22 21:05:20 +0100

    adjust link and button styling

--- extension/tailwind.config.js
+++ extension/tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {
       colors: {
         "mb-red": "#c50e20",
+        "mb-red-light": "#cf293a",
       },
       boxShadow: {
         custom: "0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24)",
