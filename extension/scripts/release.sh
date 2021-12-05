45d00f5a62a12d94a4fed8762c3798f6df91b922
Marcel Reppenhagen <marcelreppi@gmail.com>
2021-12-05 18:50:29 +0100

    create release script

---
+++ extension/scripts/release.sh
@@ -0,0 +1,20 @@
+cd ..
+
+# Get version
+echo "Reading version from manifest.json..."
+EXT_VERSION=$(cat src/manifest.json | grep \"version\" | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
+echo "Detected version $EXT_VERSION"
+echo -e "\n"
+
+# Check if extension was built
+if [ ! -f "moodle-buddy.zip" ]; then
+  echo "moodle-buddy.zip does not exist."
+  exit
+fi
+
+mkdir tmp
+cp moodle-buddy.zip "tmp/moodle-buddy-v$EXT_VERSION.zip"
+
+gh release create "v$EXT_VERSION" --notes-file "release-notes/v$EXT_VERSION.md" --title "Version $EXT_VERSION" "tmp/moodle-buddy-v$EXT_VERSION.zip"
+
+rm -rf tmp
