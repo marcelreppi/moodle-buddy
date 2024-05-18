9d05327376deba46f5e1df53ca3484d579ae3e7a
marcelreppi <marcelreppi@gmail.com>
2024-05-19 01:42:20 +0300

    Fix footer styling

--- website/components/sections/Footer.vue
+++ website/components/sections/Footer.vue
@@ -1,5 +1,7 @@
 <template>
-	<footer class="mb-10 px-4">
-		<NuxtLink to="/privacy">Privacy Statement</NuxtLink>
-	</footer>
+  <section>
+    <footer>
+      <NuxtLink to="/privacy">Privacy Statement</NuxtLink>
+    </footer>
+  </section>
 </template>
